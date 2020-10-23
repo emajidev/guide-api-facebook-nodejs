0f2033347342b0fb06bb87599bcbfe970f0cc40f
emajidev <emajidev@gmail.com>
2020-10-23 06:11:34 -0400

    instagram integration

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var https = require("https");
 var express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "/public/"));
+//instagram const declaration
+const Instagram = require("./Instagram.js");
+const username = "emajidev@gmail.com";
+const password = "Ejimenez93*";
+const client = new Instagram({ username, password });
 
+app.use(express.static(__dirname + "/public/"));
 https
   .createServer(
     {
@@ -16,3 +21,27 @@ https
   .listen("3000", function () {
     console.log("Server running in 3000 port");
   });
+
+app.get("/login_instagram", async function (req, res) {
+  await client.login();
+  const profile = await client.getProfile();
+  res.send(profile);
+});
+
+app.get("/test", async function (req, res) {
+  var nombre = req.query.nombre || "epa";
+  res.send(`fetch,${nombre}`);
+  console.log(nombre);
+});
+
+app.get("/upload_ig_photo", async function (req, res) {
+  const photo = "public/images/test.jpg"; //relative path or url, for example "public/images/test.jpg"
+  // Upload Photo to feed or story, just configure 'post' to 'feed' or 'story'
+  const { media } = await client.uploadPhoto({
+    photo: photo,
+    caption: "testing",
+    post: "feed",
+  });
+  console.log(`https://www.instagram.com/p/${media.code}/`);
+  res.send("upload photo in instagram");
+});
--- public/main.js
+++ public/main.js
@@ -21,6 +21,11 @@ async function ig_id() {
   let igID = await useApiFacebook(pageId, pageToken, action, method);
   return igID;
 }
+async function peticion_server(path) {
+  let resp = await fetch(path);
+  let response = await resp.text();
+  console.log(response);
+}
 
 $("#btn-login").click(() => {
   login();
@@ -35,8 +40,6 @@ $("#btn-pages").click(async () => {
   console.log(data);
 });
 
-///////////////////////// POST SECTION ///////////////////////////////
-// get user permissions
 $("#btn-get-permissions").click(async () => {
   let userId = localStorage.getItem("userId");
   let access_token = localStorage.getItem("access_token");
@@ -46,17 +49,7 @@ $("#btn-get-permissions").click(async () => {
   console.table(resp);
 });
 
-//Get the Instagram Business Account's Media
-$("#btn-get-data-instagram").click(async () => {
-  let dataPage = await page_id();
-  let igID = await ig_id();
-  let pageToken = dataPage.access_token;
-  let action = "/media?";
-  let method = "GET";
-  let resp = await useApiFacebook(igID, pageToken, action, method);
-  console.table(resp);
-});
-
+///////////////////////// FACEBOOK     //////////////////
 ///////////////////////// POST SECTION ///////////////////////////////
 
 // Facebook
@@ -80,13 +73,47 @@ $("#btn-publish-photos-facebook").click(async () => {
 
 $("#btn-publish-videos-facebook").click(() => {});
 
-//Instagram
-$("#btn-publish-comment-instagram").click(async () => {
-  let dataPage = await page_id();
-  let igID = await ig_id();
-  let pageToken = dataPage.access_token;
-  let action = "/feed?message= Aqui va escrito un mensaje";
-  let method = "POST";
-  let resp = await useApiFacebook(pageId, pageToken, action, method);
-  console.log(resp);
+/////////////////////////// INSTAGRAM    ////////////////
+/////////////////////////// LOGIN SECTION ///////////////
+function template(data) {
+  return `
+    <div>
+      <h1>${data.header}</h1>
+      <p>${data.subheader}</p>
+      <div id="progressBarOuter">
+        <input id="username" placeholder="Usuario" ></input>
+        <h>
+        <input id="password" placeholder="Constraseña"></input>
+      </div>
+      <Button type="submit" >Entrar</Button>
+    </div>
+  `;
+}
+
+$("#btn-login-instagram").click(async () => {
+  let newWindow = window.open(
+    "",
+    "Instagram",
+    "height=670,width=512,left=512,location=yes,menubar=no,resizable=no,scrollbars=yes,status=no,titlebar=yes,top=0"
+  );
+  let temp = template({ header: "INSTAGRAM", subheader: "Inicio de sesion" });
+  newWindow.document.write(temp);
+
+  var url = "test";
+  var data = { username: "example" };
+
+  fetch("test", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  //peticion_server("login_instagram");
+});
+
+/////////////////////////// POST SECTION ////////////////
+$("#btn-publish-ig-photo").click(async () => {
+  peticion_server("upload_ig_photo");
 });
